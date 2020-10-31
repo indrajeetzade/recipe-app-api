680f5c5e4f31fc58bab7c952c5e79885268b3873
indrajeetzade <indrajeetzade@gmail.com>
2020-10-31 09:18:15 +0530

    User api added

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,8 +1,9 @@
-from user.views import CreateUserView,CreateTokenView
+from user.views import CreateUserView,CreateTokenView,ManageUserView
 from django.urls import path
 app_name='user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(),name='create'),
     path('token/', CreateTokenView.as_view(),name='token'),
+    path('me/', ManageUserView.as_view(),name='me'),
 ]
 
