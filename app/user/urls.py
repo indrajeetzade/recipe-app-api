c0a5b3dc216b8400953f1599dcdbb661bf0fc972
indrajeetzade <indrajeetzade@gmail.com>
2020-10-31 08:26:15 +0530

    User authenticated, updated

---
+++ app/user/urls.py
@@ -0,0 +1,8 @@
+from user.views import CreateUserView,CreateTokenView
+from django.urls import path
+app_name='user'
+urlpatterns = [
+    path('create/', CreateUserView.as_view(),name='create'),
+    path('token/', CreateTokenView.as_view(),name='token'),
+]
+
